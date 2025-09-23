19edfa1fb5bb2efe81b2c41a64f93a563163ea5f
Emerson <emerson.day@gmail.com>
2025-09-22 19:15:04 -0700

    uploader

---
+++ src/Uploader.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { fetchAuthSession } from 'aws-amplify/auth';
+
+
+const API_URL = import.meta.env.VITE_API_URL 
+
+function Uploader() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState('');
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setUploadStatus('');
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      setUploadStatus('Please select a file first.');
+      return;
+    }
+
+    setUploadStatus('Getting upload URL...');
+
+    try {
+      // get token
+      const session = await fetchAuthSession();
+      const jwtToken = session.tokens.idToken.toString();
+
+      // fetch presigned url
+      const response = await fetch(`${API_URL}/generate-upload-url?fileName=${selectedFile.name}`, {
+        method: 'POST',
+        headers: {
+          'Authorization': jwtToken,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to get upload URL.');
+      }
+
+      const presignedUrl = await response.text();
+      setUploadStatus('Uploading file...');
+
+      // upload direct to s3 with presigned url
+      const uploadResponse = await fetch(presignedUrl, {
+        method: 'PUT',
+        body: selectedFile,
+        headers: {
+          'Content-Type': selectedFile.type,
+        },
+      });
+
+      if (!uploadResponse.ok) {
+        throw new Error('File upload failed.');
+      }
+
+      setUploadStatus('Upload successful!');
+    } catch (error) {
+      setUploadStatus(`Error: ${error.message}`);
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Upload Your Audio File</h2>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {uploadStatus && <p>{uploadStatus}</p>}
+    </div>
+  );
+}
+
+export default Uploader;
